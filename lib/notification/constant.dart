60a668e320892b6d07b03c1bb615b7135af394fc
9979Gyu <yuqin1161@gmail.com>
2024-01-18 07:53:43 +0800

    removed noti fcm

--- lib/notification/constant.dart
+++
@@ -1,7 +0,0 @@
-class Constant{
-
-  static final String BASE_URL = 'https://fcm.googleapis.com/fcm/send';
-  static final String KEY_SERVER = 'AAAALr-80oA:APA91bH3KnrOeDXzATGSMBLxKsn3cAwimvv2iYkNBm3fK7b3iaVY_OK6nHDqR9i3PZrz9qur7gm8Y9ajYn1aGRbsQPyWA7NLQ7a_AAVKwQGct0orypxxVRFCADJ1uCusvLOvByM4NEZB';
-  static final String SENDER_ID = '200785318528';
-
-}
